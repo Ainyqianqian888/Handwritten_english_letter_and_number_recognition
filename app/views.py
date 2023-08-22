acd1db3a77e7c06ee716fe702ff949d9e829f9d6
Ainyqianqian888 <Jicongmin@outlook.com>
2023-08-23 01:45:00 +0800

    commit

--- app/views.py
+++ app/views.py
@@ -33,9 +33,6 @@ __pred1_image_file = 'app/image/pred1.png'  # 预测结果1图片
 # __pred2_image_file = './app/image/pred2.png'  # 预测结果2图片
 # __pred3_image_file = './app/image/pred3.png'  # 预测结果3图片
 
-
-
-
 def predictPrepare():
     # init model
     model = ResNet_inference((28, 28, 1), num_classes, 0.5)
